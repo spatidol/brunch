0f9ccd863aa91916ee9ed0fb9e6f461a2f57c690
Allie Spatidol <spatidol@gmail.com>
2017-02-28 02:30:00 -0800

    fixed port issue

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get('/api/brunchplace/:brunchId', function(req, res){
 //   else res.json(aBrunch);
 // });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Server listening on port', 3000);
 });
 
